e1c40225cfce6e8d6e90b897c50a7f0c5dac7a47
Ceecee1713 <156458571+Ceecee1713@users.noreply.github.com>
2025-07-05 17:48:09 -0600

    Finished Dealer Animations - Not Fully Optimized
    The animation is done and separation of sprites was done so it uses an SO now
    
    Made DealerSprites script, edited DealerShuffleAnim script, made PracticeDealer2 scene, edited PracticeAnimating scene (renamed it to PracticeDealer), organization of scenes through folders, added more tester art and deleted unneeded ones

--- Assets/Scripts/DealerShuffleAnim.cs
+++ Assets/Scripts/DealerShuffleAnim.cs
@@ -3,156 +3,197 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
-//Move all dealer sprites into an SO
-//Modify so there'll be another image representing the cards to be slid across the table
-//So when the player collects the card, the left hand can go back to resting poisiton and its resting sprite
-
 public class DealerShuffleAnim : MonoBehaviour
 {
+    [SerializeField]
+    private DealerSprites dealerSprites;
+
+    [Header ("Card Decks")]
+    [SerializeField]
+    private GameObject playerCardDeck;
+    [SerializeField]
+    private GameObject shuffledCardDeck;
+    [SerializeField] 
+    private Transform spawnShuffledCardDeckTransform; 
+    [SerializeField]
+    private float maxHeightFromEndCardPosition = 600.0f;
+
     [Header ("Player's Hand")]
     [SerializeField]
     private GameObject playerHand;
+    [SerializeField] //Remove later
+    private Transform playerHandRestingTransform; //In the future, grab these transforms from Start method where these game objects are in the correct position
 
     [Header ("Dealer's Right Hand")] 
     [SerializeField]
-    private GameObject rightHand;
+    private GameObject rightHand; //Used to be behind "leftHand" to shuffle the cards
     [SerializeField]
-    private Sprite restingRightHandSprite;
+    private GameObject secondRightHand; //Used to be on top of "playerCardDeck" and "shuffledCardDeck" (image sorting layering)
+    [SerializeField] //Remove later
+    private Sprite restingRightHandSprite; //In the future, grab these from Start method where these game objects are in the correct position
+
+    [Header ("Dealer's Right Hand's Transforms")] 
     [SerializeField] //Remove later
     private Transform rightHandRestTransform; //In the future, grab these transforms from Start method where these game objects are in the correct position
     [SerializeField]
     private Transform shufflingHandTransform;
     [SerializeField]
-    private Transform grabCardsTransform;
+    private Transform reachForCardStackTransform;
     [SerializeField]
-    private Transform spawnCardsTransform, endCardSlideTransform;
+    private Transform endShuffledCardDeckSlideTransform;
     [SerializeField]
-    private float maxHeightFromstartingShufflingHandTransformYPosition = 200.0f;
+    private float maxHeightFromShufflingHandTransformYPosition = 200.0f;
 
     [Header ("Dealer's Left Hand")]
     [SerializeField]
     private GameObject leftHand;
-    [SerializeField]
-    private Sprite restingLeftHandSprite;
+    [SerializeField] //Remove later
+    private Sprite restingLeftHandSprite; //In the future, grab these from Start method where these game objects are in the correct position
     [SerializeField] //Remove later
     private Transform leftHandRestTransform; //In the future, grab these transforms from Start method where these game objects are in the correct position
+    [SerializeField]
+    private Transform nonShufflingHandTransform; //Position left hand will take to hold the cards when the dealer is shuffling
 
-    [Header ("Dealer's Hand Sprites")]
+    [Header ("Duration and Delays")]
     [SerializeField]
-    private Sprite reachingHand;
-    [SerializeField]
-    private Sprite shufflingHand, nonShufflingHand;
-    [SerializeField]
-    private Sprite holdingCards, passingCards;
-    [SerializeField]
-    private Sprite cardDeck;
-
-    [Header ("Duration and Delay Times")]
-    [SerializeField]
-    private float durationBetweenShuffling;
+    private float delayBetweenShuffles;
     [SerializeField]
     private float durationToSlideCards;
     [SerializeField]
-    private float delayForShuffleAndReset; //Delay before shuffling animation starts and when to reset images and their positions
+    private float delayBetweenClips; //Delay before resetting images and their positions
 
-    private Image rightHandImage;
-    private Image leftHandImage;
+    private Image leftHandImage, rightHandImage, secondRightHandImage;
 
-    private Transform leftHandTransform, rightHandTransform;
+    private Transform leftHandTransform, rightHandTransform, shuffledCardDeckTransform;
     private Transform playerRestingTransform;
 
-    private Tween firstTween, secondTween;
+    private Tween firstTween;
 
     private float startingShufflingHandTransformYPosition;
     private float newShufflingHandTransformYPosition;
-    private float endCardSlideYPosition;
-    private float offScreenYPosition; //For player's hand
+    private float endCardSlideYPosition, receivedCardYPosition;
+    private float offScreenPlayerHandYPosition; //For player's hand
 
-    private int maxNumberOfShufflingLoops = 2;
+    private int maxNumberOfShufflingLoops = 4;
 
-    void Start()
+    void Start() //Have both of the dealer's hands at their starting positions before play
     {
         rightHandImage = rightHand.GetComponent<Image>();
+        secondRightHandImage = secondRightHand.GetComponent<Image>();
         leftHandImage = leftHand.GetComponent<Image>();
 
         playerRestingTransform = playerHand.GetComponent<Transform>();
         leftHandTransform = leftHand.GetComponent<Transform>();
         rightHandTransform = rightHand.GetComponent<Transform>(); 
+        shuffledCardDeckTransform = shuffledCardDeck.GetComponent<Transform>();
 
         //rightHandRestTransform.position = rightHandTransform.position; //Add back in later
         //leftHandRestTransform.position = leftHandTransform.position; //Add back in later
 
-        //Setting Y values for the right hand, where cards will stop sliding at, and the player's hand at a resting position
+        //Setting Y values
         startingShufflingHandTransformYPosition = shufflingHandTransform.position.y; 
-        newShufflingHandTransformYPosition = startingShufflingHandTransformYPosition + maxHeightFromstartingShufflingHandTransformYPosition;
-        endCardSlideYPosition = endCardSlideTransform.position.y;
-        offScreenYPosition = playerRestingTransform.position.y;
+        newShufflingHandTransformYPosition = startingShufflingHandTransformYPosition + maxHeightFromShufflingHandTransformYPosition;
+        endCardSlideYPosition = endShuffledCardDeckSlideTransform.position.y;
+        offScreenPlayerHandYPosition = playerRestingTransform.position.y;
+        receivedCardYPosition = endCardSlideYPosition + maxHeightFromEndCardPosition;
 
         StartCoroutine(DealerShufflingCards()); //Call coroutine on events and button click
     }
 
     IEnumerator DealerShufflingCards()
     {
-        //The dealer's right hand will reach for the cards
-        rightHandImage.sprite = reachingHand;
-        rightHand.transform.position = new Vector3(grabCardsTransform.position.x, grabCardsTransform.position.y, grabCardsTransform.position.z);
-        yield return new WaitForSeconds(delayForShuffleAndReset);
+        //Above, slide up the player down / invoke event to allow player input
+
+
+        //Player returns cards
+        playerHand.transform.position = new Vector3(playerHand.transform.position.x, endCardSlideYPosition, playerHand.transform.position.z); //Choppily grab cards
+        shuffledCardDeck.transform.position = new Vector3(playerHand.transform.position.x, endCardSlideYPosition + 200f, playerHand.transform.position.z); //Choppily move cards 
+        shuffledCardDeck.SetActive(true);
+        yield return new WaitForSeconds(delayBetweenClips);
+
+
+        //Dealer will grab cards given by the player
+        playerHand.transform.position = playerHandRestingTransform.position;
+        secondRightHand.transform.position = shuffledCardDeck.transform.position;
+        secondRightHandImage.sprite = dealerSprites.RightHandReachingOver;
+        playerCardDeck.transform.position = shuffledCardDeck.transform.position;
+        rightHand.SetActive(false);
+        shuffledCardDeck.SetActive(false);
+        secondRightHand.SetActive(true);
+        playerCardDeck.SetActive(true);
+        yield return new WaitForSeconds(delayBetweenClips);
+
+
+        //Dealer will slide the given cards up
+        playerCardDeck.transform.position = new Vector3(playerHand.transform.position.x, receivedCardYPosition, playerHand.transform.position.z); //Choppily move cards upwards
+        secondRightHand.transform.position = new Vector3(playerHand.transform.position.x, receivedCardYPosition, playerHand.transform.position.z); //Choppily move hand upwards
+        yield return new WaitForSeconds(delayBetweenClips);
+
+
+        //The dealer's right hand will reach for the cards to shuffle
+        rightHand.SetActive(true);
+        secondRightHand.SetActive(false);
+        rightHandImage.sprite = dealerSprites.RightHandGrabbingCards;  
+        rightHand.transform.position = new Vector3(reachForCardStackTransform.position.x, reachForCardStackTransform.position.y, reachForCardStackTransform.position.z);
+        yield return new WaitForSeconds(delayBetweenClips);
+
 
         //The dealer's hands will be in position and have correct sprites for shuffling the cards
+        playerCardDeck.SetActive(false);
         rightHand.transform.position = new Vector3(shufflingHandTransform.position.x, shufflingHandTransform.position.y, shufflingHandTransform.position.z);
-        rightHandImage.sprite = shufflingHand;
-        leftHandImage.sprite = nonShufflingHand;
+        rightHandImage.sprite = dealerSprites.RightHandShuffling;
+        leftHand.transform.position = nonShufflingHandTransform.position;
+        leftHandImage.sprite = dealerSprites.LeftHandShuffling;    
+        yield return new WaitForSeconds(delayBetweenClips);
 
-        yield return new WaitForSeconds(delayForShuffleAndReset);
+
 
         //The dealer will choppily shuffle the cards 
         for(int numberOfShufflingLoops = 0; numberOfShufflingLoops < maxNumberOfShufflingLoops; numberOfShufflingLoops++)
         {
             rightHand.transform.position = new Vector3(shufflingHandTransform.position.x, startingShufflingHandTransformYPosition, shufflingHandTransform.position.z);
-            yield return new WaitForSeconds(durationBetweenShuffling);
+            yield return new WaitForSeconds(delayBetweenShuffles);
             rightHand.transform.position = new Vector3(shufflingHandTransform.position.x, newShufflingHandTransformYPosition, shufflingHandTransform.position.z);
-            yield return new WaitForSeconds(durationBetweenShuffling);
+            yield return new WaitForSeconds(delayBetweenShuffles);
         }
 
         rightHand.transform.position = new Vector3(shufflingHandTransform.position.x, startingShufflingHandTransformYPosition, shufflingHandTransform.position.z);
-        yield return new WaitForSeconds(durationBetweenShuffling);
+        yield return new WaitForSeconds(delayBetweenShuffles);
 
 
-        //Below, modify so there'll be another image representing the cards to be slid across the table
-        //So when the player collects the card, the left hand can go back to resting poisiton and its resting sprite
+        //The dealer will prepare to slide out the cards
+        secondRightHand.transform.position = rightHand.transform.position;
+        secondRightHandImage.sprite = dealerSprites.RightHandHoldingCards;
+        rightHand.SetActive(false);
+        secondRightHand.SetActive(true);
+        leftHandImage.sprite = dealerSprites.LeftHandHoldingCards;
+        yield return new WaitForSeconds(delayBetweenClips);
 
-
-        leftHand.SetActive(false);
-        rightHandImage.sprite = holdingCards;
-        yield return new WaitForSeconds(durationBetweenShuffling);
 
         //The dealer will slide out cards 
-        rightHandImage.sprite = passingCards;
-        leftHand.transform.position = spawnCardsTransform.position;
-        leftHandImage.sprite = cardDeck;
-        leftHand.SetActive(true);
-        firstTween = leftHandTransform.DOMoveY(endCardSlideYPosition, durationToSlideCards, false);
+        secondRightHandImage.sprite = dealerSprites.RightHandPassingCards;
+        shuffledCardDeck.transform.position = spawnShuffledCardDeckTransform.position;
+        shuffledCardDeck.SetActive(true);
+        firstTween = shuffledCardDeckTransform.DOMoveY(endCardSlideYPosition, durationToSlideCards, false); //Smoothly slide out the cards
         yield return firstTween.WaitForCompletion();
 
 
-        //Player's hand will collect the cards
-        firstTween = playerHand.transform.DOMoveY(endCardSlideYPosition, durationToSlideCards, false);
+        //Player's hand will collect the cards and dealer's hands will reset to normal sprites and resting positions
+        playerHand.transform.position = new Vector3(playerHand.transform.position.x, endCardSlideYPosition, playerHand.transform.position.z); //Choppily grab cards
         rightHandImage.sprite = restingRightHandSprite;
         rightHand.transform.position = rightHandRestTransform.position;
-        //Set left hand to go back to its resting position and resting sprite, in this line
-
-
-        yield return firstTween.WaitForCompletion();
-        firstTween = playerHand.transform.DOMoveY(offScreenYPosition, durationToSlideCards, false);
-        secondTween = leftHandTransform.transform.DOMoveY(offScreenYPosition, durationToSlideCards, false);
-
-        yield return new WaitForSeconds(delayForShuffleAndReset);
-
-
-        //Remove later
+        rightHand.SetActive(true);
+        secondRightHand.SetActive(false);
+        secondRightHand.transform.position = rightHandRestTransform.position;
         leftHandImage.sprite = restingLeftHandSprite;
         leftHand.transform.position = leftHandRestTransform.position;
+        yield return new WaitForSeconds(delayBetweenClips);
 
+
+        //Reset player's hand and card deck image
+        playerHand.transform.position = playerHandRestingTransform.position; //Choppily move hand back 
+        shuffledCardDeck.SetActive(false);
+        shuffledCardDeck.transform.position = spawnShuffledCardDeckTransform.position;
 
         //Below, slide up the player cards / invoke event to allow player input
     }
